49d6a093aa2a64e2e6eff3a03010105378a54034
Shesh <sheshnathverma@gmail.com>
2018-08-26 23:55:31 +0100

    core interface of JsORM
    core interface of JsORM

---
+++ ISerializable.ts
@@ -0,0 +1,8 @@
+export namespace JsORM {
+    export interface ISerializable {
+        readonly LastUpdated: Date;
+        toJson(depth?: number): any;
+        fromJson(json: any, depth?: number): void;
+        cast<T extends ISerializable>(target: T, depth?: number): T;
+    }
+}
