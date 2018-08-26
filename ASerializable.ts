f061deacb4f72a7d34fc52c72c47f4ac74dd98cc
Shesh <sheshnathverma@gmail.com>
2018-08-27 00:28:22 +0100

    remove namespace

--- ASerializable.ts
+++ ASerializable.ts
@@ -1,64 +1,65 @@
-export namespace JsORM {
-    export abstract class ASerializable implements ISerializable {
-        private Loaded: boolean;
-        get IsLoaded(): boolean {
-            return this.Loaded;
-        }
+import { ISerializable } from './ISerializable';
 
-        get LastUpdated(): Date {
-            return this.Get("LastUpdated");
-        }
 
-        get HasChanged(): boolean {
-            return !!this.Get("HasChanged");
-        }
+export abstract class ASerializable implements ISerializable {
+    private Loaded: boolean = false;
+    get IsLoaded(): boolean {
+        return this.Loaded;
+    }
 
-        protected Map: Map<string, any>;
-        protected Json: any = {};
+    get LastUpdated(): Date {
+        return this.Get("LastUpdated");
+    }
 
-        constructor() {
-            this.Json = {};
-            this.Map = new Map();
-            this.Update({ DateTime: new Date(), HasChanged: false });
-        }
+    get HasChanged(): boolean {
+        return !!this.Get("HasChanged");
+    }
 
-        protected Get(Name: string, format?: Function) {
-            format = format || function (v) { return v };
-            return format(this.Map.get(Name));
-        }
+    protected Map: Map<string, any>;
+    protected Json: any = {};
 
-        protected Set(Name: string, value: any, parser?: Function): boolean {
-            parser = parser || function (v) { return v };
-            if (!Object.is(this.Map.get(Name), parser(value))) {
-                this.Map.set(Name, parser(value));
-                this.Update({});
-                return true;
-            }
-            return false;
-        }
+    constructor() {
+        this.Json = {};
+        this.Map = new Map();
+        this.Update({ DateTime: new Date(), HasChanged: false });
+    }
 
-        protected Update({ DateTime, HasChanged }: any): void {
-            this.Map.set("LastUpdated", DateTime || new Date());
-            this.Map.set("HasChanged", HasChanged === true);
-        }
+    protected Get(Name: string, format?: Function) {
+        format = format || function (v: any) { return v };
+        return format(this.Map.get(Name));
+    }
 
-        toJson(depth?: number): any {
-            let self = {};
-            self["LastUpdated"] = this.Get("LastUpdated");
-            return self;
-        }
-
-        fromJson(json: any, depth?: number): void {
-            this.Json = json || {};
-            this.Map = new Map();
-            this.Loaded = true;
+    protected Set(Name: string, value: any, parser?: Function): boolean {
+        parser = parser || function (v: any) { return v };
+        if (!Object.is(this.Map.get(Name), parser(value))) {
+            this.Map.set(Name, parser(value));
             this.Update({});
+            return true;
         }
+        return false;
+    }
 
-        cast<T extends ISerializable>(target: T, depth?: number): T {
-            depth = depth || 0;
-            target.fromJson(this.toJson(depth));
-            return target;
-        }
+    protected Update({ DateTime, HasChanged }: any): void {
+        this.Map.set("LastUpdated", DateTime || new Date());
+        this.Map.set("HasChanged", HasChanged === true);
+    }
+
+    toJson(depth?: number): any {
+        let self: any = {};
+        self["LastUpdated"] = this.Get("LastUpdated");
+        return self;
+    }
+
+    fromJson(json: any, depth?: number): void {
+        this.Json = json || {};
+        this.Map = new Map();
+        this.Loaded = true;
+        this.Update({});
+    }
+
+    cast<T extends ISerializable>(target: T, depth?: number): T {
+        depth = depth || 0;
+        target.fromJson(this.toJson(depth));
+        return target;
     }
 }
